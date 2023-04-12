85e1cfc8fe147bfc8948264f21a9eabaca28c398
Mjolksaft <Kalla01@hotmail.se>
2023-04-12 21:08:51 +0200

    added authentication

--- backend/server.js
+++ backend/server.js
@@ -52,3 +52,26 @@ app.post("/login", async (req, res) => {
         res.sendStatus(404)
     }
 })
+
+function checkRole(role) {
+    return function(req, res, next) {
+      const userRole = req.user.role
+      console.log(userRole, role);
+      if (!role.includes(userRole)) {
+        return res.status(403).json({message: 'Forbidden you dont have access to that resource'})
+      } 
+      next()
+    }
+}
+
+function authenticateUser(req, res, next) {
+    console.log(req);
+    const authHeader = req.headers.authorization
+    const token = authHeader.split(' ')[1]
+    if(token == null) return res.sendStatus(403)
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
+        if(err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
