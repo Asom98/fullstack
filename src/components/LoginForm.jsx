9b6e8643a65dd12d280e5c0bcc8f9df2182b179d
Arro <103262516+AruneeThongkrathok@users.noreply.github.com>
2023-04-26 02:17:01 +0200

    added a backend connection for LoginForm

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,22 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginForm.css";
 
+export const Login = ({ onClose, onLogin }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginStatus, setLoginStatus] = useState(null);
 
-export const Login = ({ onClose }) => {
-  const handleLoginSubmit = (event) => {
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLoginSubmit = async (event) => {
     event.preventDefault();
-    //  Handle login logic here!
+
+    try {
+      const response = await fetch("http://localhost:5000/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setLoginStatus(true);
+        onLogin(data[0].accessToken, data[1]);
+        onClose();
+      } else {
+        setLoginStatus(false);
+      }
+    } catch (error) {
+      console.error("Error submitting login form:", error);
+      setLoginStatus(false);
+    }
   };
 
   return (
     <div className="login-form-container">
-      <form onSubmit={handleLoginSubmit}>
-        <label for="email">Email</label>
-        <input type="email" placeholder="youremail@gmail.com" id="email" name="email" required />
-        <label for="password">Password</label>
-        <input type="password" placeholder="********" id="password" name="password" required />
-        <button type="submit">Log In</button>
-      </form>
+        <form onSubmit={handleLoginSubmit}>
+            <label htmlFor="username">Username</label>
+            <input
+            type="text"
+            placeholder="username"
+            id="username"
+            name="username"
+            value={username}
+            onChange={handleUsernameChange}
+            required
+            />
+            <label htmlFor="password">Password</label>
+            <input
+            type="password"
+            placeholder="********"
+            id="password"
+            name="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+            />
+            <button type="submit">Log In</button>
+        </form>
+        {loginStatus !== null && (
+            <p className={`message ${loginStatus ? "success" : "error"}`}>
+            {loginStatus ? "Login Successful" : "Login Failed"}
+            </p>
+        )}
     </div>
   );
 };
