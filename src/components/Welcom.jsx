9b322b6a89bb6b4234a3c85e3f92952b12767e8f
Arro <103262516+AruneeThongkrathok@users.noreply.github.com>
2023-04-30 21:05:34 +0200

    updated the start page. added background picture and a textbox

--- src/components/Welcom.jsx
+++ src/components/Welcom.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import "./Welcome.css";
 
 export const Welcom = () => {
   return (
-    <div className="d-flex justify-content-center fs-1 fst-italic font-monospace">Full stack</div>
+    <div className="d-flex justify-content-left main-page">
+      <div className="frame">
+        <p>Welcome to our beauty salon!</p>
+        <p>
+        We offer a wide range of services that cater to all genders and identities! Whether you're looking for a fresh new haircut, a relaxing massage, or a rejuvenating facial, our skilled professionals are here to make you look and feel your best. We use only the highest quality products and the latest techniques to achieve stunning results that will leave you feeling pampered and refreshed. So why wait? Contact us today for a personal consultation and let us help you unleash your inner beauty!
+        </p>
+      </div>
+    </div>
   )
 }
