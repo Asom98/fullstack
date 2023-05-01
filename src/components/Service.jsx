e591df80ab439c70800d11efdaeed68bd5c17032
Arro <103262516+AruneeThongkrathok@users.noreply.github.com>
2023-05-01 11:15:49 +0200

    updated style on service page

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -5,18 +5,18 @@ import "./Service.css";
 
 export const ServicePage = () => {
     return (
-        <div className="d-flex flex-column justify-content-center align-items-center">
-            <div className="d-flex justify-content-center fs-1 fst-italic font-monospace">Our Services</div>
+        <div className=" container d-flex flex-column justify-content-center align-items-center">
+            <div className="d-flex justify-content-center fs-1 fst-italic font-monospace text-white mt-5">Our Services</div>
             <div className="card-columns mt-4 custom-card-columns">
                 {servicesData.map((service) => (
                     <div key={service._id} className="custom card">
                         <Card>
                             <Card.Body>
                                 <Card.Title>{service.name}</Card.Title>
-                                <Card.Subtitle className="mb-2 text-muted">{service.employee_name}</Card.Subtitle>
+                                <Card.Subtitle className="employee-text">{service.employee_name}</Card.Subtitle>
                                 <Card.Text>{service.details}</Card.Text>
                                 <Card.Text>Price: {service.price}</Card.Text>
-                                <Button variant="primary">Book Now</Button>
+                                <Button>Book Now</Button>
                             </Card.Body>
                         </Card>
                     </div>
