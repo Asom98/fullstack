fa0838eccc7b55d7c1ff828b6c57227a99a0e549
Arro <103262516+AruneeThongkrathok@users.noreply.github.com>
2023-05-01 12:38:02 +0200

    modified style of start page

--- src/components/Welcom.jsx
+++ src/components/Welcom.jsx
@@ -6,7 +6,7 @@ export const Welcom = () => {
   return (
     <div className="main-page">
       <div className="frame">
-        <p>Welcome to our beauty salon!</p>
+        <p className="welcome-text">Welcome to our beauty salon!</p>
         <p>
         We offer a wide range of services that cater to all genders and identities! Whether you're looking for a fresh new haircut, a relaxing massage, or a rejuvenating facial, our skilled professionals are here to make you look and feel your best. We use only the highest quality products and the latest techniques to achieve stunning results that will leave you feeling pampered and refreshed. So why wait? Contact us today for a personal consultation and let us help you unleash your inner beauty!
         </p>
