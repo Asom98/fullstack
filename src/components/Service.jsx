b5b93674a7d2dcc4358ed5ad5bf05a191a7c596a
Arro <103262516+AruneeThongkrathok@users.noreply.github.com>
2023-05-04 18:48:48 +0200

    get data from service api and display them

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Card, Button } from "react-bootstrap";
-import servicesData from "../json/services.json";
 import "./css/Service.css";
+import axios from "axios";
 
 export const ServicePage = () => {
+  const [servicesData, setServicesData] = useState([]);
+
+  useEffect(() => {
+    const fetchServices = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/services/getServices");
+        setServicesData(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchServices();
+  }, []);
+
   return (
     <div className=" service-container d-flex flex-column justify-content-center align-items-center">
       <div className="d-flex justify-content-center fs-1 fst-italic font-monospace text-white mt-5">
@@ -14,11 +28,9 @@ export const ServicePage = () => {
           <div key={service._id} className="custom card">
             <Card>
               <Card.Body>
-                <Card.Title>{service.name}</Card.Title>
-                <Card.Subtitle className="employee-text">
-                  {service.employee_name}
-                </Card.Subtitle>
-                <Card.Text>{service.details}</Card.Text>
+                <Card.Title className="card-serviceName">{service.name}</Card.Title>
+                <Card.Text className="card-description">{service.description}</Card.Text>
+                <Card.Text>Duration: {service.duration}min</Card.Text>
                 <Card.Text>Price: {service.price}</Card.Text>
                 <Button>Book Now</Button>
               </Card.Body>
