9cebb9363ef31806c5ef76815f29ccc41f4b3325
Arro <103262516+AruneeThongkrathok@users.noreply.github.com>
2023-05-05 17:03:02 +0200

    updated the userpage so user can modify their info

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,6 +1,7 @@
-import React from "react";
-import { Button } from "react-bootstrap";
+import React, {useState} from "react";
+import { Button,Card } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import "./css/User.css"
 
 function User() {
   const navigate = useNavigate();
@@ -11,15 +12,94 @@ function User() {
     window.location.reload();
   };
 
-  const user = JSON.parse(localStorage.getItem("user"));
+  const [email, setEmail] = useState(user.email);
+  const [phoneNumber, setPhoneNumber] = useState(user.phoneNumber);
+  const [editEmailMode, setEditEmailMode] = useState(false);
+  const [editPhoneNumberMode, setEditPhoneNumberMode] = useState(false);
+
+  const handleUpdateEmailClick = async () => {
+    const response = await fetch("http://localhost:3000/admin/updateUser", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: user._id,
+        email,
+      }),
+    });
+
+    if (response.ok) {
+      setEditEmailMode(false);
+      const updatedUser = { ...user, email };
+      localStorage.setItem("user", JSON.stringify(updatedUser));
+      setEmail(email);
+    }
+  };
+
+  const handleUpdatePhoneNumberClick = async () => {
+    const response = await fetch("http://localhost:3000/admin/updateUser", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: user._id,
+        phoneNumber,
+      }),
+    });
+
+    if (response.ok) {
+      setEditPhoneNumberMode(false);
+      const updatedUser = { ...user, phoneNumber };
+      localStorage.setItem("user", JSON.stringify(updatedUser));
+      setPhoneNumber(phoneNumber);
+    }
+  };
 
   return (
-    <div>
-      <h1>Welcome, {user.username}!</h1>
-      <p>Email: {user.email}</p>
-      <p>Phone Number: {user.phoneNumber}</p>
+    <div className="container">
+      <h1 className="welcome-text">Welcome, {user.username}!</h1>
+      <Card>
+        <Card.Body>
 
-      <Button variant="primary" onClick={handleLogoutClick}>Logout</Button>
+          <Card.Subtitle className="mb-2 text-muted">
+            Email:{" "} {editEmailMode ? (
+            <span>
+              <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+              <Button variant="primary" onClick={handleUpdateEmailClick}>Save</Button>
+              <Button variant="secondary" onClick={() => setEditEmailMode(false)}> Cancel </Button>
+            </span>
+
+            ) : (
+
+              <span>
+                {user.email}{" "}
+                <Button variant="link" onClick={() => setEditEmailMode(true)}> Edit </Button>
+              </span>
+            )}
+
+            Phone Number:{" "}
+            {editPhoneNumberMode ? (
+              <span>
+                <input type="text" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)}/>
+                <Button variant="primary" onClick={handleUpdatePhoneNumberClick}>Save</Button>
+                <Button variant="secondary" onClick={() => setEditPhoneNumberMode(false)}>Cancel</Button>
+              </span>
+
+            ) : (
+
+              <span>
+                {user.phoneNumber}{" "}
+                <Button variant="link" onClick={() => setEditPhoneNumberMode(true)}>Edit</Button>
+              </span>
+            )}
+          </Card.Subtitle>
+          <div className="logout-button">
+            <Button variant="danger" onClick={handleLogoutClick}> Logout </Button>
+          </div>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
