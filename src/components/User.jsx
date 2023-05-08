48b50c6a204158ab32a5382a2522bc6d56ad83a5
Mjolksaft <Kalla01@hotmail.se>
2023-05-08 21:57:53 +0200

    cleaned up the code in user

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,23 +1,18 @@
 import React, {useState,useEffect} from "react";
-import { Button,Card, Table, Modal } from "react-bootstrap";
+import { Button,Card } from "react-bootstrap";
+import { useNavigate } from "react-router";
 import "./css/User.css"
-import { ConfirmationModal } from "./parts/ConfirmationModal";
 
 function User() {
-
-  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
-  const [bookingToDelete, setBookingToDelete] = useState(null);
-  const [showModal, setShowModal] = useState(false);
-  const [registrationSentence, setRegistrationSentence] = useState("");
-
   const user = JSON.parse(localStorage.getItem("user"));
 
-  const [email, setEmail] = useState(user.email);
-  const [phoneNumber, setPhoneNumber] = useState(user.phoneNumber);
+  const navigate = useNavigate();
+
   const [editEmailMode, setEditEmailMode] = useState(false);
   const [editPhoneNumberMode, setEditPhoneNumberMode] = useState(false);
   const [bookings, setBookings] = useState([]);
-
+  const [userInfo, setUserInfo] = useState({})
+ 
   const handleUpdateEmailClick = async () => {
     const response = await fetch("http://localhost:3000/admin/updateUser", {
       method: "PUT",
@@ -26,15 +21,12 @@ function User() {
       },
       body: JSON.stringify({
         id: user._id,
-        email,
+        email: userInfo.email,
       }),
     });
 
     if (response.ok) {
       setEditEmailMode(false);
-      const updatedUser = { ...user, email };
-      localStorage.setItem("user", JSON.stringify(updatedUser));
-      setEmail(email);
     }
   };
 
@@ -45,23 +37,22 @@ function User() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        id: user._id,
-        phoneNumber,
+        id: userInfo._id,
+        phoneNumber: userInfo.phoneNumber,
       }),
     });
 
     if (response.ok) {
       setEditPhoneNumberMode(false);
-      const updatedUser = { ...user, phoneNumber };
-      localStorage.setItem("user", JSON.stringify(updatedUser));
-      setPhoneNumber(phoneNumber);
     }
   };
 
-  const handleUserBookings = async () => {
-    const response = await fetch(`http://localhost:3000/bookings/getBookingsByUserId/${user._id}`, {
+  const handleUserBookings = async (token) => {
+
+    const response = await fetch(`http://localhost:3000/bookings/getBookingsByUserId`, {
       method: "GET",
       headers: {
+        "authorization": `Bearer ${token}`,
         "Content-Type": "application/json",
       },
     });
@@ -92,132 +83,91 @@ function User() {
     }
   };
 
+  const fetchUserData = async (token) => {
+    const response = await fetch(`http://localhost:3000/users/getUserData`, {
+      method: "GET",
+      headers: {
+        "authorization": `Bearer ${token}`
+      }
+    });
+    
+    if (response.ok) {
+      const result = await response.json()
+      setUserInfo(result)
+      return
+    }
+
+    if (response.status === 403) {
+      navigate("/login")
+    }
+  };
+  
   useEffect(() => {
-    handleUserBookings();
-    console.log(bookings)
+    const token = localStorage.getItem("token")
+
+    if (token == null) {
+      navigate("/login")
+    };
+
+    handleUserBookings(token);
+    fetchUserData(token);
   }, []);
 
-  const handleDeleteBooking = async (_id) => {
-    setBookingToDelete(_id);
-    setShowConfirmationModal(true);
-  };
-
-  const handleConfirmDeleteBooking = async () => {
-    if (bookingToDelete) {
-      const packet = { _id: bookingToDelete };
-      const response = await fetch(`http://localhost:3000/bookings/deleteBooking`, {
-        method: "DELETE",
-        body: JSON.stringify(packet),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
-      if (response.ok) {
-        const updatedBookings = bookings.filter((booking) => booking._id !== bookingToDelete);
-        setBookings(updatedBookings);
-      } else {
-        setRegistrationSentence("I'm sorry, you are unable to delete your booking within 24 hours of the scheduled time. Please contact the salon via phone to make any necessary changes. Thank you for your understanding.");
-        setShowModal(true);
-      }
-    }
-    setShowConfirmationModal(false);
-  };
-
-  const handleCancelDeleteBooking = () => {
-    setBookingToDelete(null);
-    setShowConfirmationModal(false);
-  };
 
   return (
-    <div className="container-fluid">
+    <div className="container">
+      <h1 className="welcome-text">Welcome, {userInfo.username}!</h1>
       <Card>
         <Card.Body>
-          <div className="welcome-text">
-            <h1>Welcome, {user.username}!</h1>
-          </div>
-          <div className="user-details">
-            <Table>
-              <tbody>
-                <tr>
-                  <td>Email:</td>
-                  <td>
-                    {editEmailMode ? (
-                      <span>
-                        <input className="email-input" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                        <Button variant="primary" size="sm" onClick={handleUpdateEmailClick}> Save </Button>
-                        <Button variant="secondary" size="sm" onClick={() => setEditEmailMode(false)}> Cancel </Button>
-                      </span>
-                    ) : (
-                      <span>
-                        {user.email}{" "}
-                        <Button variant="link" onClick={() => setEditEmailMode(true)}> Edit </Button>
-                      </span>
-                    )}
-                  </td>
-                </tr>
-                <tr>
-                  <td>Phone Number:</td>
-                  <td>
-                    {editPhoneNumberMode ? (
-                      <span>
-                        <input className="phoneNumber-input" type="text" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
-                        <Button variant="primary" size="sm" onClick={handleUpdatePhoneNumberClick}> Save </Button>
-                        <Button variant="secondary" size="sm" onClick={() => setEditPhoneNumberMode(false)}> Cancel </Button>
-                      </span>
-                    ) : (
-                      <span>
-                        {user.phoneNumber}{" "}
-                        <Button variant="link" onClick={() => setEditPhoneNumberMode(true)}> Edit </Button>
-                      </span>
-                    )}
-                  </td>
-                </tr>
-              </tbody>
-            </Table>
-          </div>
-          <div className="bookings">
-            <h3>Your Bookings:</h3>
-            {bookings.length > 0 ? (
-              <Table className="bookings-table" bordered hover>
-                <thead>
-                  <tr>
-                    <th>Service</th>
-                    <th>Date</th>
-                    <th>Time</th>
-                    <th>Delete</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {bookings.map((booking) => (
-                    <tr key={booking._id}>
-                      <td>{booking.service ? booking.service.name : "unknown"}</td>
-                      <td>{booking.bookingDate}</td>
-                      <td>{booking.bookingTime}</td>
-                      <td>
-                        <Button variant="danger" size="sm" onClick={()=> handleDeleteBooking(booking._id)}>Cancel booking</Button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
+
+          <Card.Subtitle className="mb-2 text-muted text-center">
+            Email:{" "} {editEmailMode ? (
+            <span>
+              <input type="email" value={userInfo.email} onChange={(e) => setUserInfo({ ...userInfo, email: e.target.value })} />
+              <Button variant="primary" size="sm" onClick={handleUpdateEmailClick}>Save</Button>
+              <Button variant="secondary" size="sm" onClick={() => setEditEmailMode(false)}> Cancel </Button>
+            </span>
+
             ) : (
-              <p>You have no bookings.</p>
+
+              <span>
+                {userInfo.email}{" "}
+                <Button variant="link" onClick={() => setEditEmailMode(true)}> Edit </Button>
+              </span>
             )}
-            <Modal className="cancelBooking-modal" show={showConfirmationModal} onHide={handleCancelDeleteBooking}>
-              <Modal.Header closeButton>
-                <Modal.Title>Confirm Delete Booking</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>Are you sure you want to cancel this booking?</Modal.Body>
-              <Modal.Footer>
-                <Button variant="secondary" onClick={handleCancelDeleteBooking}>No</Button>
-                <Button variant="danger" onClick={handleConfirmDeleteBooking}>Yes</Button>
-              </Modal.Footer>
-            </Modal>
-            {showModal && (
-            <ConfirmationModal className="confirmationModal" sentance={registrationSentence} onClose={() => setShowModal(false)}/>
+
+            Phone Number:{" "}
+            {editPhoneNumberMode ? (
+              <span>
+                <input type="text" value={userInfo.phoneNumber} onChange={(e) => setUserInfo({ ...userInfo, phoneNumber: e.target.value })}/>
+                <Button variant="primary" size="sm" onClick={handleUpdatePhoneNumberClick}>Save</Button>
+                <Button variant="secondary" size="sm" onClick={() => setEditPhoneNumberMode(false)}>Cancel</Button>
+              </span>
+
+            ) : (
+
+              <span>
+                {userInfo.phoneNumber}{" "}
+                <Button variant="link" onClick={() => setEditPhoneNumberMode(true)}>Edit</Button>
+              </span>
             )}
-          </div>
+          </Card.Subtitle>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <Card.Title>My Bookings</Card.Title>
+          {bookings.length > 0 ? (
+          <ul>{bookings.map((booking) => (
+            <li key={booking._id}>
+            Service: {booking.service ? booking.service.name : "unknown"} <br />
+            Date: {booking.bookingDate} <br />
+            Time: {booking.bookingTime} <br />
+          </li>))}
+          </ul>
+          ) : (
+          <p>You have no bookings yet.</p>
+          )}
         </Card.Body>
       </Card>
     </div>
