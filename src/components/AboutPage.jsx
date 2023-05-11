294e6bad7eb9f175cb38853b4f89ea2213fba496
Fera 13 <fera6055@gmail.com>
2023-05-11 20:53:25 +0200

    Fixed about style

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -4,103 +4,119 @@ import "./css/About.css";
 
 export function AboutPage() {
   return (
-    <Container className="about-container justify-content-center">
-      <Container className="about-in-container justify-content-center rounded-5">
+    <Container className="about-container">
+      <Container className="about-in-container rounded-circle">
         <h2 className="team-title">FrontEnd Team:</h2>
-        <Row className="developer-row rounded-5">
-          <Col xs={12} md={3}>
-            <Container className="developer-pic-Farah rounded-5 mt-4 mb-4"></Container>
-          </Col>
-          <Col xs={12} md={9} className="mb-5 mt-5">
-            <h3>Farah Mallah</h3>
-            <p>
-              - Creted, styled and made Admin page responsive <br />{" "}
-              &nbsp;&nbsp;&nbsp;&nbsp;* Made Admin control the users on the
-              frontend:
-              <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;* Made Admin control the other admins on
-              the frontend.
-              <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;* Made Admin control the bookings on the
-              frontend.
-              <br />- Creted, styled and made Registration page responsive:
-              <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;* Creted checking for the registration
-              fileds.
-              <br />- Creted, styled and made Booking page responsive.
-              <br />- Creted, styled and made About page responsive.
-            </p>
+        <Row className="developer-row rounded-circle">
+          <Col xs={12}>
+            <Container className="developer-pic-Farah rounded-circle"></Container>
+            <Container className="about-info rounded-circle">
+              <h2>Farah Mallah</h2>
+              <p>
+                Creted, styled and made Admin page responsive
+                <br /> <span style={{ color: "green" }}>*</span> Made Admin
+                control the users on the frontend
+                <br />
+                <span style={{ color: "green" }}>*</span> Made Admin control the
+                other admins on the frontend
+                <br />
+                <span style={{ color: "green" }}>*</span> Made Admin control the
+                bookings on the frontend
+                <br />
+                Creted, styled and made Registration page responsive
+                <br />
+                <span style={{ color: "green" }}>*</span> Creted checking for
+                the registration fileds
+                <br />
+                Creted, styled and made Booking page responsive
+                <br />
+                Creted, styled and made About page responsive
+              </p>
+            </Container>
           </Col>
         </Row>
-        <Row className="developer-row rounded-5">
-          <Col xs={12} md={3}>
-            <Container className="developer-pic rounded-5 mt-4 mb-4"></Container>
-          </Col>
-          <Col xs={12} md={9} className="mb-5 mt-5">
-            <h3>Arrunee</h3>
-            <p>
-              - Created, styled and made Navigation bar responsive: <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;* Rerouted to different pages <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;* Added different buttons depending on the
-              state of the user like login, logout, register buttons
-              <br />- Created, styled and made Login modal. <br />- Created,
-              styled and made Home page responsive.
-              <br />- Created, styled and made User page responsive: <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;* Fetched user information and bookings
-              from backend and displayed it <br />
-              - Created, styled and made Service page responsive: <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;* Fetched services from backend <br />-
-              Created, styled and made Contact us page responsive: <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;* Displayed the map using Google Maps API
-              links.
-            </p>
+        <Row className="developer-row rounded-circle">
+          <Col xs={12}>
+            <Container className="developer-pic rounded-circle mt-4 mb-4"></Container>
+            <Container className="about-info rounded-circle">
+              <h2>Arrunee</h2>
+              <p>
+                Made Navigation bar
+                <br />
+                Created, styled and made Login modal
+                <br />
+                Created, styled and made Home page responsive
+                <br />
+                Created, styled and made User page responsive
+                <br />
+                <span style={{ color: "green" }}>*</span> Fetched user
+                information and bookings from backend and displayed it <br />
+                Created, styled and made Service page responsive
+                <br />
+                <span style={{ color: "green" }}>*</span> Fetched services from
+                backend <br />
+                Created, styled and made Contact us page responsive
+                <br />
+                <span style={{ color: "green" }}>*</span> Displayed the map
+                using Google Maps API links.
+              </p>
+            </Container>
           </Col>
         </Row>
         <h2 className="team-title">BackEnd Team:</h2>
-        <Row className="developer-row rounded-5">
-          <Col xs={12} md={3}>
-            <Container className="developer-pic-david rounded-5 mt-4 mb-4"></Container>
-          </Col>
-          <Col xs={12} md={9} className="mb-5 mt-5">
-            <h3>David Kalla</h3>
-            <p>- Booking system</p>
-            <p>- Security</p>
-            <p>- Database</p>
-          </Col>
-        </Row>
-        <Row className="developer-row rounded-5">
-          <Col xs={12} md={3}>
-            <Container className="developer-pic-Simon rounded-5 mt-4 mb-4"></Container>
-          </Col>
-          <Col xs={12} md={9} className="mb-5 mt-5">
-            <h3>Simon</h3>
-            <p>
-              - Planning initial backend and database structure
-              <br />- Initial basic registration implementation. No involvement
-              in auth middleware, finished implementation made by David.
-              <br />- Services <br />- Database management:
-              <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;* Adding multiple developers for Atlas
-              access <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;* Assisting frontend team with database
-              issues <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;* Service table data management <br />-
-              Assisting with render deployment <br />- Facebook login feature
-            </p>
+        <Row className="developer-row rounded-circle">
+          <Col xs={12}>
+            <Container className="developer-pic-david rounded-circle mt-4 mb-4"></Container>
+            <Container className="about-info rounded-circle">
+              <h2>David Kalla</h2>
+              <p>
+                Booking system
+                <br />
+                Security
+                <br />
+                Database
+              </p>
+            </Container>
           </Col>
         </Row>
-        <Row className="developer-row rounded-5">
-          <Col xs={12} md={3}>
-            <Container className="developer-pic-Kassim rounded-5 mt-4 mb-4"></Container>
+        <Row className="developer-row rounded-circle">
+          <Col xs={12}>
+            <Container className="developer-pic-Simon rounded-circle mt-4 mb-4"></Container>
+            <Container className="about-info rounded-circle">
+              <h2>Simon Eriksson</h2>
+              <p>
+                Planning initial backend and database structure
+                <br />
+                Initial basic registration implementation. Finished
+                implementation made by David.
+                <br />
+                Services <br />
+                Database management:
+                <br />
+                <span style={{ color: "green" }}>*</span> Adding multiple
+                developers for Atlas access <br />
+                <span style={{ color: "green" }}>*</span> Assisting frontend
+                team with database issues <br />
+                <span style={{ color: "green" }}>*</span> Service table data
+                management <br />
+                Assisting with render deployment <br />
+                Facebook login feature
+              </p>
+            </Container>
           </Col>
-          <Col xs={12} md={9} className="mb-5 mt-5">
-            <h3>Kassem Alsheikh</h3>
-            <p>
-              - Creating a functional model for the website <br />
-              - Making the admin functional in the backend <br />
-              - Created a mailer to send confirmations to the users <br />-
-              Making APIs for: admin, statistics, etc
-            </p>
+        </Row>
+        <Row className="developer-row rounded-circle">
+          <Col xs={12}>
+            <Container className="developer-pic-Kassim rounded-circle mt-4 mb-4"></Container>
+            <Container className="about-info rounded-circle">
+              <h2>Kassem Alsheikh</h2>
+              <p>
+                Creating a functional model for the website <br />
+                Making the admin functional in the backend <br />
+                Created a mailer to send confirmations to the users <br />
+                Making APIs for: admin, statistics, etc
+              </p>
+            </Container>
           </Col>
         </Row>
       </Container>
