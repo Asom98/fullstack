808b1ba4a2c5de85c3e24c2c1b6ab188e7848fa8
Fera 13 <fera6055@gmail.com>
2023-05-13 23:47:12 +0200

    Added a footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Welcom } from "./components/Welcom";
 import { ServicePage } from "./components/Service";
 import "./main.css";
-import { Routes, Route,  } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Registration } from "./components/Registration";
 import { Login } from "./components/LoginForm";
 import NavigationBar from "./components/NavigationBar";
@@ -12,28 +12,34 @@ import { AboutPage } from "./components/AboutPage";
 import ContactUs from "./components/ContactUs";
 import ProtectedRoute from "./components/parts/ProtectedRoute";
 import React, { Fragment, useState } from "react";
+import { Footer } from "./components/parts/Footer";
 
 function App() {
-
   return (
     <div>
       <NavigationBar />
       <main className="App">
-      <Routes>
-          <Route element={<ProtectedRoute/>}>  {/* checks if they have an active token if not they will be redirected to the login screen */}
+        <Routes>
+          <Route element={<ProtectedRoute />}>
+            {" "}
+            {/* checks if they have an active token if not they will be redirected to the login screen */}
             <Route path="/user" element={<User />} />
             <Route path="/admin" element={<AdminPage />} />
             <Route path="/booking/:_id" element={<Booking />} />
           </Route>
-          
+
           <Route path="/" element={<Welcom />} />
           <Route path="/services" element={<ServicePage />} />
-          <Route path="/registration" element={<Registration isAdmin={false} />} />
+          <Route
+            path="/registration"
+            element={<Registration isAdmin={false} />}
+          />
           <Route path="/login/:showPopup" element={<Login />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contactus" element={<ContactUs />} />
-      </Routes>
+        </Routes>
       </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/parts/Footer.jsx
@@ -0,0 +1,39 @@
+import "../css/Footer.css";
+export function Footer() {
+  return (
+    <footer class="my-footer text-center bg-gradient p-3">
+      <row class="row">
+        <column class="col">
+          Contact us:
+          <row class="row">
+            <column class="col">Phone: +46123456789</column>
+          </row>
+          <row class="row">
+            <column class="col">Email: info@example.com</column>
+          </row>
+        </column>
+        <column class="col text-center">
+          <p class="my-row">Adress:</p>
+          <p>Elmetorpsvägen 15, 291 39 Kristianstad</p>
+        </column>
+        <column class="col">
+          <row>Find us on social media</row>
+          <row class="row">
+            <column class="col">
+              <button class="insta" type="button"></button>
+            </column>
+            <column class="col">
+              <button class="facebook" type="button"></button>
+            </column>
+            <column class="col">
+              <button class="twitter" type="button"></button>
+            </column>
+          </row>
+          <row class="row">
+            <column class="col">© HKR BEAUTY SALON, 2023</column>
+          </row>
+        </column>
+      </row>
+    </footer>
+  );
+}
