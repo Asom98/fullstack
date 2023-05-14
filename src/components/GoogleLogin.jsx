471e0997c6df1ced01e5a11764675cac71b83135
Simon Eriksson <simon_eriksson_90@outlook.com>
2023-05-14 15:38:44 +0200

    user can now be added to db

--- src/components/GoogleLogin.jsx
+++ src/components/GoogleLogin.jsx
@@ -1,15 +1,32 @@
 import { useEffect } from 'react';
 
-function handleCredentialResponse(response) {
-  console.log('Encoded JWT', response.credential);
+async function handleCredentialResponse(userData) {
+  console.log('Encoded JWT', userData.credential);
   // decode jwt 
-  const jwt = response.credential;
+  const jwt = userData.credential;
   const jwtParts = jwt.split('.');
   const payload = JSON.parse(atob(jwtParts[1]));    // deprecated decode function but works for now
  
   console.log('Decoded JWT payload', payload);
-  
-  // Do something with the credential, such as send it to your server for verification
+  // send decoded jwt to backend
+  const packet = {
+    username: payload.name,
+    password: payload.sub,
+    email: payload.email,
+    phoneNumber: payload.phone_number,
+  };
+  const response = await fetch("http://localhost:3000/users/register", {
+    method: "POST",
+    body: JSON.stringify(packet),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.status === 201) {
+    console.log("You have succesfuly registered!");
+  } else {
+    console.log("This user already exists");
+  }
 }
 
 function initializeGoogleLogin() {
