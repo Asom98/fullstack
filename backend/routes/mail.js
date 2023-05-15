8733cde6e1962f82fa8f7322a8ac256719a0aad2
Mjolksaft <Kalla01@hotmail.se>
2023-05-15 15:19:15 +0200

    Fixed mail bug when deleting before mail

--- backend/routes/mail.js
+++ backend/routes/mail.js
@@ -6,7 +6,7 @@ const bookingModel = require("../models/booking");
 const employeeModel = require("../models/employee")
 const userModel = require("../models/user");
 
-async function confirmBooking(bookingId) {
+async function confirmBooking(booking, user) {
     let config = {
         service: "gmail",
         auth: {
@@ -16,19 +16,17 @@ async function confirmBooking(bookingId) {
         }
     }
     let transporter = nodemailer.createTransport(config)
-    const currBooking = await bookingModel.findById(bookingId)
-    const currUser = await userModel.findById(currBooking.user_id)
-    const currService = await serviceModel.findById(currBooking.service_id)
-    const currEmployee = await employeeModel.findById(currBooking.employee_id)
+    const currUser = await userModel.findById(user._id)
+    const currService = await serviceModel.findById(booking.service_id)
+    const currEmployee = await employeeModel.findById(booking.employee_id)
     
-  
     let message = {
         from: process.env.EMAIL,
         to: `${currUser.email}`,
         subject: "Booking confirmation!",
         text: `Hello ${currUser.username} here comes your booking confirmation.
     
-    Date: ${currBooking.startTime}.
+    Date: ${booking.startTime}.
     Service: ${currService.name}.
     Service provider: ${currEmployee.name}.
   
