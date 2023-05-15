442cf780fa025414084d666fbe43408a078cd1e1
Mjolksaft <Kalla01@hotmail.se>
2023-05-15 17:00:13 +0200

    added correct url

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -28,7 +28,7 @@ export const Login = (props) => {
     event.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:3000/users/login", {
+      const response = await fetch("https://backend-saloon.onrender.com/users/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
