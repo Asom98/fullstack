9c040ac408122aa6a7746ea7088706a64bb493c6
Mjolksaft <Kalla01@hotmail.se>
2023-05-15 15:56:28 +0200

    added useEFfcet rom react

--- src/components/parts/ChangePassword.jsx
+++ src/components/parts/ChangePassword.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Modal, Form } from "react-bootstrap";
 
 function ChangePassword(props) {
