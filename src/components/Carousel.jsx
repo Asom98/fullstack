3b5c2d0c7196623d9ca6632d3c0b6f3db752fc64
Fera 13 <fera6055@gmail.com>
2023-05-16 12:18:30 +0200

    Fixed home page responsivness

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,45 +1,57 @@
 import Carousel from "react-bootstrap/Carousel";
 import "./css/Carousel.css";
 import { Link } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import {
+  Form,
+  Button,
+  Container,
+  Row,
+  Col,
+  Card,
+  Accordion,
+} from "react-bootstrap";
 
 function CarouselComp() {
   return (
     <Carousel slide interval={2000}>
       <Carousel.Item>
         <div className="carousel-image pic1" />
-        <div className="carousel-text">
-          <h2>Introducing our New Beard Grooming Service!</h2>
-          <p>
+        <Container className="carousel-text">
+          <h2 class="overflow-text">
+            Introducing our New Beard Grooming Service!
+          </h2>
+          <p class="overflow-text">
             Attention all bearded gentlemen! We are thrilled to announce that we
             now offer a comprehensive beard grooming service, complete with
             specialized beard care treatments.
           </p>
-          <p>
+          <p class="overflow-text">
             Book your appointment today and let our team of skilled barbers take
             care of your beard grooming needs.
           </p>
           <Button as={Link} to="/services" className="register-button">
             Go to Services
           </Button>
-        </div>
+        </Container>
       </Carousel.Item>
       <Carousel.Item>
         <div className="carousel-image pic2" />
-        <div className="carousel-text">
+        <Container className="carousel-text">
           <h2>Summer Special Offer!</h2>
           <h3>10% off on all facials</h3>
-          <p>Please note that the discount will be applied at our salon.</p>
-        </div>
+          <p class="overflow-text">
+            Please note that the discount will be applied at our salon.
+          </p>
+        </Container>
       </Carousel.Item>
       <Carousel.Item>
-        <div className="carousel-image pic3">
-          <div className="carousel-text">
-            <h2>
+        <div class="carousel-image pic3">
+          <Container className="carousel-text">
+            <h2 class="overflow-text">
               Join our membership program today and get exclusive access to
               discounts, rewards, and special offers!
             </h2>
-            <p>
+            <p class="overflow-text">
               As a member, you'll receive early access to our latest products
               and events, and after you've booked services worth $500 with us,
               you'll get a coupon worth $15 to use on any chosen service! Click
@@ -50,21 +62,21 @@ function CarouselComp() {
             <Button as={Link} to="/registration" className="register-button">
               Register Now!
             </Button>
-          </div>
+          </Container>
         </div>
       </Carousel.Item>
       <Carousel.Item>
-        <div className="carousel-image pic4" />
-        <div className="carousel-text">
-          <h2>
+        <div class="carousel-image pic4" />
+        <Container className="carousel-text">
+          <h2 class="overflow-text">
             At our salon, we offer eyelash services with the best quality hair
             on the market.
           </h2>
-          <h6>
+          <h6 class="overflow-text">
             Our expert technicians are dedicated to providing you with the most
             luxurious and natural-looking lashes possible.
           </h6>
-        </div>
+        </Container>
       </Carousel.Item>
     </Carousel>
   );
